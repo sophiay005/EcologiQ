63b8c14318d174b21329f74732f1f8c9abedfd78
윤소희 <yoonsophia@yunsohuiui-MacBookPro.local>
2022-03-31 17:22:53 +0900

    ky

--- app/src/main/java/sohee/yoon/ecologiq/ElateBeauty.java
+++ app/src/main/java/sohee/yoon/ecologiq/ElateBeauty.java
@@ -1,6 +1,12 @@
 package sohee.yoon.ecologiq;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +16,31 @@ public class ElateBeauty extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.elatebeauty);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+
+
+        Button buttonlinkprepprimer = findViewById(R.id.buttonlinkprepprimer);
+        buttonlinkprepprimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myWebView.loadUrl("https://www.naver.com/");
+                WebSettings webSettings = myWebView.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                myWebView.setWebViewClient(new WebViewClientClass());
+            }
+        });
+
     }
+
+    private class WebViewClientClass extends WebViewClient {//페이지 이동
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.d("check URL",url);
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
 }
--- app/src/main/java/sohee/yoon/ecologiq/FatAndTheMoon.java
+++ app/src/main/java/sohee/yoon/ecologiq/FatAndTheMoon.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class VyanaPlantBeauty extends AppCompatActivity {
+public class FatAndTheMoon extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vyanaplantbeauty);
+        setContentView(R.layout.fatandthemoon);
     }
 }
