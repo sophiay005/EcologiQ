e930fb5d5f57a6c92c14949e68710977997d7433
sophiay005 <sophiay005@gmail.com>
2022-04-06 18:47:56 +0900

    안뇽 4/6/22

--- app/src/main/java/sohee/yoon/ecologiq/ElateBeauty.java
+++ app/src/main/java/sohee/yoon/ecologiq/ElateBeauty.java
@@ -1,8 +1,10 @@
 package sohee.yoon.ecologiq;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -12,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ElateBeauty extends AppCompatActivity {
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +38,9 @@ public class ElateBeauty extends AppCompatActivity {
 
     private class WebViewClientClass extends WebViewClient {//페이지 이동
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            Log.d("check URL",url);
-            view.loadUrl(url);
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest wrr) {
+            Log.d("check URL", String.valueOf(wrr.getUrl()));
+            view.loadUrl(String.valueOf(wrr.getUrl()));
             return true;
         }
     }
